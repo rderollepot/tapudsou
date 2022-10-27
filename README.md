c54425966965f11924461802f9ff8e035b620071
Bertrand Richard <bertrand.richard@univ-eiffel.fr>
2022-10-27 13:13:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Tapudsou
+
+Python script that tells you when you need to add money to your lunch card.
+
+## Installation
+
+You need to add your credentials to the Windows Credential Locker, under the `tapudsou` identifier.
+
+## Usage
+
+```
+usage: main.py [-h] username threshold
+
+positional arguments:
+  username
+  threshold   Account balance in € below which to alert
+
+options:
+  -h, --help  show this help message and exit
+```
---
+++ main.py
@@ -0,0 +1,51 @@
+import ctypes
+import argparse
+import webbrowser
+
+import requests
+import keyring
+
+
+def message_box(title, text, style):
+    return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('username')
+    parser.add_argument('threshold', type=float, help='Account balance in € below which to alert')
+    args = parser.parse_args()
+    pwd = keyring.get_password("tapudsou", args.username)
+
+    if not pwd:
+        message_box("Tapudsou", f"Impossible de trouver le mot de passe", 0)
+        return
+
+    headers = {
+        "password": pwd,
+        "rememberMe": "false",
+        "grant_type": "password",
+        "brandId": 1249,
+        "username": args.username,
+    }
+
+    with requests.Session() as s:
+        r = s.post("https://api.innovorder.fr/oauth/login", headers)
+        rr = r.json()
+        customer_id = rr['data']['customer']['customerId']
+        headers = {
+            "authorization": f"Bearer {rr['access_token']}"
+        }
+
+        r = s.get(f"https://api.innovorder.fr/customers/{customer_id}/balance", headers=headers)
+        rr = r.json()
+        balance = rr['data']['customerBalance']
+        balance = float(balance) / 100.
+
+        if balance < args.threshold:
+            message_box("Tapudsou", f"Il reste {balance}€ sur ta carte de cantine!", 0)
+            webbrowser.open_new_tab("https://ewallet.innovorder.fr/1249/home")
+
+
+if __name__ == '__main__':
+    main()
